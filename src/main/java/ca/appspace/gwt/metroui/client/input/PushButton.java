3ee14565fa901d46a15fe1bdb2e1d6fad3c7c789
Eugene Sukharev <eugenes.mobile@gmail.com>
2014-04-28 23:21:12 -0400

    PushButton support added

---
+++ src/main/java/ca/appspace/gwt/metroui/client/input/PushButton.java
@@ -0,0 +1,90 @@
+package ca.appspace.gwt.metroui.client.input;
+
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.ButtonBase;
+
+public class PushButton extends ButtonBase {
+
+	private Size _size;
+	private Style _style;
+
+	public PushButton() {
+		super(Document.get().createPushButtonElement());
+	}
+
+	public PushButton(String html) {
+		this();
+		setHTML(html);
+	}
+	
+	public PushButton(String html, ClickHandler handler) {
+		this(html);
+		addClickHandler(handler);
+	}
+	
+	public PushButton setSize(Size size) {
+		_size = size;
+		setStyleName();
+		return this;
+	}
+
+	public PushButton setStyle(Style style) {
+		_style = style;
+		setStyleName();
+		return this;
+	}
+
+	private void setStyleName() {
+		StringBuilder sb = new StringBuilder();
+		if (_size!=null) {
+			sb.append(_size.asHtmlValue());
+		}
+		
+		if (_style!=null) {
+			if (sb.length()>0) {
+				sb.append(" ");
+			}
+			sb.append(_style.asHtmlValue());
+		}
+		setStyleName(sb.toString());
+	}
+
+
+	public static enum Size {
+		LARGE("large"), 
+		SMALL("small"), 
+		MINI("mini");
+		
+		private final String _htmlValue;
+		
+		private Size(String value) {
+			_htmlValue = value;
+		}
+		
+		public String asHtmlValue() {
+			return _htmlValue;
+		}
+	}
+	
+	public static enum Style {
+		DEFAULT("default"), 
+		PRIMARY("primary"),
+		INFO("info"),
+		SUCCESS("success"),
+		WARNING("warning"),
+		DANGER("danger"),
+		INVERSE("inverse"), 
+		LINK("link");
+	
+		private final String _htmlValue;
+		
+		private Style(String value) {
+			_htmlValue = value;
+		}
+		
+		public String asHtmlValue() {
+			return _htmlValue;
+		}
+	}
+}
