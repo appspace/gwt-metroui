556cff9e563c8d0ca7841131f49ccb5d86a7a67a
Eugene Sukharev <eugenes.mobile@gmail.com>
2014-05-06 18:02:05 -0400

    Library initialization

--- src/main/java/ca/appspace/gwt/metroui/client/MetroUI.java
+++ src/main/java/ca/appspace/gwt/metroui/client/MetroUI.java
@@ -4,24 +4,47 @@ import ca.appspace.gwt.metroui.client.input.Calendar;
 import ca.appspace.gwt.metroui.client.navigation.NavBar;
 import ca.appspace.gwt.metroui.client.navigation.TabControl;
 
+import com.google.gwt.core.shared.GWT;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class MetroUI {
 
-	public static native void initAll() /*-{
-		$wnd.$.Metro.initAll();
-	}-*/;
+	public static void prepareHTML() {
+		RootPanel body = RootPanel.get();
+		if (body==null) {
+			Window.alert("Please include <body> element in html");
+			return;
+		}
+		if (!body.getElement().hasClassName("metro")) {
+			GWT.log("Adding class='metro' annotation to <body> element");
+			body.addStyleName("metro");
+		}
+	}
 
 	public static void init(Calendar widget) {
-		initCalendar(widget.getElement().getParentElement());
+		try {
+			initCalendar(widget.getElement().getParentElement());
+		} catch (Exception e) {
+			GWT.log("Unable to initialize Calendar. Are you sure /js/metro.min.js is included in your HTML?", e);
+		}
 	}
 	
 	public static void init(TabControl widget) {
-		initTabControl(widget.getElement().getParentElement());
+		try {
+			initTabControl(widget.getElement().getParentElement());
+		} catch (Exception e) {
+			GWT.log("Unable to initialize Calendar. Are you sure /js/metro.min.js is included in your HTML?", e);
+		}
 	}
 	
 	public static void init(NavBar navBar) {
-		initNavBar(navBar.getElement().getParentElement());
+		try {
+			initNavBar(navBar.getElement().getParentElement());
+		} catch (Exception e) {
+			GWT.log("Unable to initialize NavBar. Are you sure /js/metro.min.js is included in your HTML?", e);
+		}
 	}
 	
 	private static native void initTabControl(Element elem) /*-{
@@ -35,5 +58,7 @@ public class MetroUI {
 	private static native void initCalendar(Element elem) /*-{
 		$wnd.$.Metro.initCalendars(elem);
 	}-*/;
-	
+	public static native void initAll() /*-{
+		$wnd.$.Metro.initAll();
+	}-*/;
 }
